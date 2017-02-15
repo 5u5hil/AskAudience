b0b98586e4c7ffa1a35c1afa95392f7354795484
sdhsawant <sdhsawant@gmail.com>
2017-02-16 00:56:37 +0530

    testing

--- js/app.js
+++ js/app.js
@@ -88,7 +88,10 @@ angular.module('askaudience', ['ionic', 'ngCordova', 'askaudience.controllers', 
                         };
                     }
                 })
-
+                setTimeout(function(){
+                     window.location.href = "#app/closed_polldetails/1297";
+                },3000);
+  
                 if (window.cordova && window.cordova.plugins.Keyboard) {
                     cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
                     cordova.plugins.Keyboard.disableScroll(true);
