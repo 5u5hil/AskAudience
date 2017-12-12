4b5875200829d3b5a93959dd400d3d3c1e418876
Swapnil Patwa <swapnil@infiniteit.biz>
2017-12-12 17:04:55 +0530

    updated domain

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Ionic askaudience App
-var domain = 'http://www.askaudience.com/api/?method=';
+var domain = 'http://ask-audience.cruxservers.in/api/?method=';
 var playerId = '';
 //var domain = 'http://ask-audience.cruxservers.in/api/?method=';
 angular.module('askaudience', ['ionic', 'ngCordova', 'askaudience.controllers', 'askaudience.services', 'askaudience.directives', 'ion-datetime-picker', 'ngTagsInput', 'ion-autocomplete'])
